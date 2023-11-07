bbe9cf4c2ee0b9d6991c709479a28e3d2c84d9aa
Ira <ira@MacBook-Air-Irina.local>
2023-11-07 18:10:29 +0200

    added function is_different

--- puzzle.py
+++ puzzle.py
@@ -1,1 +1,14 @@
-
+def is_different(case: str) -> bool:
+    """
+    Function chechs whether it has the same numbers in string.
+    Returns False or True
+    >>> is_different(" 6  83  *")
+    True
+    >>> is_different("**3 3****")
+    False
+    """
+    numbers = []
+    for i in case:
+        if i.isdigit():
+            numbers.append(i)
+    return len(numbers) == len(set(numbers))
